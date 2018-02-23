04f373fdfec4a7fed25a0f2acfada04a1b28bbc9
rolaeru <olaeru.razvan@gmail.com>
2018-02-23 11:30:53 +0200

    add XML config instead of java annotation config on chuckNorrisQuotes class to make it a bean

--- src/main/java/com/pentalog/razvan/Spring5JokesApp/Spring5JokesAppApplication.java
+++ src/main/java/com/pentalog/razvan/Spring5JokesApp/Spring5JokesAppApplication.java
@@ -5,8 +5,10 @@ import com.pentalog.razvan.Spring5JokesApp.controllers.NormalJokesController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class Spring5JokesAppApplication {
 
 	public static void main(String[] args) {
